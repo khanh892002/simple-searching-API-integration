b4cf49150dbd411bd25a15be11b8cff8da569041
Khanh8902 <116865538+khanh892002@users.noreply.github.com>
2025-06-10 00:52:36 +0700

    splitting source code into server and client folder, adding a vercel.json for vercel to tell client and server's source code apart, using node on searching source code of api

---
+++ server/api/search.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+const apiKey = "54d8394b6b67e9971caa11d856c8ef673c65e48b4e468ea5fec1e1e7bf476279";
+
+module.exports = async (req, res) => {
+    const keyWord = req.query.q;
+    if (!keyWord) return res.status(400).json({error: 'Missing key word'});
+
+    try {
+        const url = `https://serpapi.com/search.json?engine=google&q=${keyWord}&api_key=${apiKey}`;
+        // const url = `https://cors-anywhere.herokuapp.com/google.com/search?${search.replace(/\s+/g, '%20')}`;
+        // not stable, there is a rate limit, easily blocked
+        const response = await fetch(url);
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({error: 'Failed to fetch from SerpAPI'});
+    }
+}
