e108e34951a63381a5b1c747a7bdf58a40dbd990
Khanh8902 <116865538+khanh892002@users.noreply.github.com>
2025-06-13 17:54:55 +0700

    forget to put PORT in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const searchRoute = require('./routes/search');
-
+const PORT = 4000;
 const app = express();
 app.use(cors());
 
 app.get('/search', searchRoute);
+
+app.listen(PORT, () => {console.log(`Server is running on ${PORT}`);});
