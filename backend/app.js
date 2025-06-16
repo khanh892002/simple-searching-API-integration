d948f76b1c91cb6e25ada220206684850e17dafb
Khanh8902 <116865538+khanh892002@users.noreply.github.com>
2025-06-16 21:34:05 +0700

    cors using wasn't necessary

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const searchRoute = require('./routes/search');
 const app = express();
-app.use(cors());
+//app.use(cors());
 
 app.get('/search', searchRoute);
 
